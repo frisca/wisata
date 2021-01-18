4d796fc77f99bfad857a17782517db5a0fcaca83
frisca <frisca.novalinda@ottodigital.id>
2021-01-19 02:16:47 +0700

    fix

--- resources/views/konfirmasi/index.blade.php
+++ resources/views/konfirmasi/index.blade.php
@@ -265,7 +265,7 @@
               <a href="{{ URL('user/konfirmasi/detail/' . $p->nomor_pemesanan) }}">
                 <button type="button" class="btn btn-warning">Detail</button>
               </a>
-              @if($p->jumlah_bayar == 0)
+              @if($p->jumlah_bayar == 0 && $p->pembayaran <= 0)
               <a href="{{ URL('user/pembayaran/' . $p->nomor_pemesanan . '/wisata/' . $p->id_wisata) }}">
                 <button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal">Selesaikan</button>
               @endif 
@@ -273,8 +273,8 @@
                 <button type="button" class="btn btn-info edit" data-toggle="modal" data-target="#myModal"
                 pemesananid="<?php echo $p->id_pemesanan;?>">Upload Bukti Bayar</button>
               @endif 
-              @if($p->pembayaran == 2)
-                @if($p->status_pemesanan == 2)
+              @if($p->pembayaran == 2 && $p->jumlah_bayar != 0)
+                @if($p->status_pemesanan == 2 )
                 <button type="button" class="btn btn-danger refund" data-toggle="modal" data-target="#myModalRefund"
                 pemesananid="<?php echo $p->nomor_pemesanan;?>">Refund</button>
                 @endif
@@ -283,7 +283,7 @@
                   <button type="button" class="btn btn-success">Reschedule</button>
                 </a>
                 @endif
-              @elseif($p->pembayaran == 1)
+              @elseif($p->pembayaran == 1 && $p->status_pemesanan == 1)
                 <button type="button" class="btn btn-primary lunas" data-toggle="modal"  data-target="#myModalLunas"
                 pemesananid="<?php echo $p->id_pemesanan;?>" total="<?php echo ($p->total - $p->jumlah_bayar);?>">Bayar Lunas</button>
               @endif 
